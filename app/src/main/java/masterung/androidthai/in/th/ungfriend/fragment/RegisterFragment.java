702f0669ec8940bdf5906ef22e3d063b4bd879b8
MasterUNG <phrombutr@gmail.com>
2018-08-01 15:26:15 +0700

    Finish 1August

--- app/src/main/java/masterung/androidthai/in/th/ungfriend/fragment/RegisterFragment.java
+++ app/src/main/java/masterung/androidthai/in/th/ungfriend/fragment/RegisterFragment.java
@@ -1,5 +1,9 @@
 package masterung.androidthai.in.th.ungfriend.fragment;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -8,11 +12,16 @@ import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import masterung.androidthai.in.th.ungfriend.MainActivity;
 import masterung.androidthai.in.th.ungfriend.R;
 
 public class RegisterFragment extends Fragment{
+
+    private Uri uri;
+    private ImageView imageView;
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
@@ -21,8 +30,51 @@ public class RegisterFragment extends Fragment{
 //        Create Toolbar
         createToolbar();
 
+//        Photo Controller
+        photoController();
+
 
     }   // Main Method
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == getActivity().RESULT_OK) {
+
+            uri = data.getData();
+
+            try {
+
+                Bitmap bitmap = BitmapFactory.decodeStream(getActivity().getContentResolver().openInputStream(uri));
+
+                Bitmap bitmap1 = Bitmap.createScaledBitmap(bitmap, 800, 600, true);
+
+                imageView.setImageBitmap(bitmap1);
+
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+
+        } else {
+            Toast.makeText(getActivity(), "Please Choose Image", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    private void photoController() {
+        imageView = getView().findViewById(R.id.imvPhoto);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType("image/*");
+                startActivityForResult(Intent.createChooser(intent, "Choose App"), 1);
+            }
+        });
+    }
 
     private void createToolbar() {
         Toolbar toolbar = getView().findViewById(R.id.toolbarRegister);
